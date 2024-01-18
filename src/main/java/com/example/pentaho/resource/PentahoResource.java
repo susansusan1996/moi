b5d64117861257034914abe8012c2d60b6b573ca
2212009 <ann.yu@iisigroup.com>
2024-01-18 10:40:56 +0800

    將baseUrl 改為 /api/batchForm/**

--- src/main/java/com/example/pentaho/resource/PentahoResource.java
+++ src/main/java/com/example/pentaho/resource/PentahoResource.java
@@ -2,8 +2,10 @@ package com.example.pentaho.resource;
 
 
 import com.example.pentaho.component.JobParams;
+import com.example.pentaho.component.User;
 import com.example.pentaho.service.JobService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 
 
-@RequestMapping("/api/kettle")
+@RequestMapping("/api/batchForm")
 @RestController
 public class PentahoResource {
 
@@ -54,7 +56,7 @@ public class PentahoResource {
      */
     @GetMapping("/test")
     public ResponseEntity<String> test() throws IOException {
-        log.info("sucess");
+        UserContextUtils.getUserHolder();
         return new ResponseEntity<>("成功", HttpStatus.OK);
     }
 
@@ -62,7 +64,7 @@ public class PentahoResource {
      * @param jobParams
      * @return
      */
-    @PostMapping("/excuteJob")
+    @PostMapping("/excuteETLJob")
     public ResponseEntity<Integer> excuteJob(@RequestBody JobParams jobParams) throws IOException {
         log.info("jobName:{}", jobParams.getJobName());
         Integer responseCode = jobService.excuteJob(jobParams);
@@ -73,11 +75,11 @@ public class PentahoResource {
     }
 
     /**
-     *
+     * 使用者傳送想比對的批次檔案到指定位置
      * @param jobParams
      * @return
      */
-    @PostMapping("/excuteTrans")
+    @PostMapping("/excuteETLTrans")
     public ResponseEntity<Integer> excuteTrans(@RequestBody JobParams jobParams) throws IOException {
         log.info("jobName:{}",jobParams.getJobName());
         Integer responseCode = jobService.excuteTrans(jobParams);
@@ -100,5 +102,4 @@ public class PentahoResource {
             return new ResponseEntity<>("fail",HttpStatus.NOT_FOUND);
         }
     }
-
 }
