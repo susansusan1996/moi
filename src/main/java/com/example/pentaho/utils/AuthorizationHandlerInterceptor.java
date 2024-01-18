d70a87bfff2fcecc8496d78088d93f6a3b1a0562
2212009 <ann.yu@iisigroup.com>
2024-01-18 10:44:01 +0800

    加入io.netty依賴，儲存使用者資訊

--- src/main/java/com/example/pentaho/utils/AuthorizationHandlerInterceptor.java
+++ src/main/java/com/example/pentaho/utils/AuthorizationHandlerInterceptor.java
@@ -2,12 +2,17 @@ package com.example.pentaho.utils;
 
 import com.example.pentaho.component.KeyComponent;
 import com.example.pentaho.component.Token;
+import com.example.pentaho.component.User;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.UserDetailsManagerConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -18,6 +23,7 @@ public class AuthorizationHandlerInterceptor implements HandlerInterceptor {
 
     private final static Logger log = LoggerFactory.getLogger(AuthorizationHandlerInterceptor.class);
     private final KeyComponent keyComponent;
+
 
     public AuthorizationHandlerInterceptor(KeyComponent keyComponent) {
         this.keyComponent = keyComponent;
@@ -66,22 +72,27 @@ public class AuthorizationHandlerInterceptor implements HandlerInterceptor {
              * 驗證使用者身分
              * 先直接給予JwtToken
              */
-            if (authHeader.substring(6, authHeader.length()) != null && "".equals(authHeader.substring(6, authHeader.length()))) {
+            if (authHeader.substring(7, authHeader.length()) != null && "".equals(authHeader.substring(6, authHeader.length()))) {
                 /**前端補403導回登入頁?**/
                 throw new ResponseStatusException(HttpStatus.FORBIDDEN, "not allowed");
             }
+        String RSATokenJwt = authHeader.substring(7, authHeader.length());
 
-            log.info("RASJWTToken:{}", authHeader.substring(6, authHeader.length()));
+        log.info("RASJWTToken:{}", RSATokenJwt);
             /**
              * 驗證RASJWTToken
              * 完成後return true 會再導向Spring-SecurityFilterChain
              */
             if(Token.fromRSAJWTToken(authHeader.substring(7, authHeader.length()),keyComponent.getKeyname())){
+                User user = Token.extractUserFromRSAJWTToken(RSATokenJwt, keyComponent.getKeyname());
+                UserContextUtils.setUserHolder(user);
                 return true;
             }
         /**其他錯誤；前端補403導回登入頁?**/
         throw new ResponseStatusException(HttpStatus.FORBIDDEN, "not allowed");
     }
+
+
 }
 
 
