8ad1300ee01b0a202dd3049049232e8f33fe7beb
SusanChung <susan.chung@iisigroup.com>
2024-01-31 18:12:33 +0800

    redis: 新增一個key對應多筆資料(RPUSH、LRANGE)程式碼

--- src/main/java/com/example/pentaho/resource/RedisResouce.java
+++ src/main/java/com/example/pentaho/resource/RedisResouce.java
@@ -4,11 +4,15 @@ import com.example.pentaho.component.SingleQueryDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/redis")
@@ -20,13 +24,28 @@ public class RedisResouce {
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
 
+
+    /**
+     * 獲取單筆(set、get)
+     */
     @PostMapping("/query-data")
     public void queryData(@RequestBody SingleQueryDTO singleQueryDTO) {
         String redisValue = stringRedisTemplate.opsForValue().get(singleQueryDTO.getRedisKey());
         log.info("redisValue: {}", redisValue);
     }
 
+    /**
+     * 獲取一個key的多筆資料(RPUSH、LRANGE )
+     */
+    @PostMapping("/query-data-list")
+    public void queryDataList(@RequestBody SingleQueryDTO singleQueryDTO) {
+        ListOperations<String, String> listOps = stringRedisTemplate.opsForList();
+        List<String> elements = listOps.range(singleQueryDTO.getRedisKey(), 0, -1);
+        log.info("redisValue: {}", elements);
+    }
 
     //用Jedis獲取redis，多線程中比較不安全
 //    @PostMapping("/query-data-by-jedis")
