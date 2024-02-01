90443dd90057e93fcd1dbee81b65ccdc889b74e9
SusanChung <susan.chung@iisigroup.com>
2024-02-01 11:09:23 +0800

    redis: 新增模糊比對(scan)程式碼

--- src/main/java/com/example/pentaho/resource/RedisResouce.java
+++ src/main/java/com/example/pentaho/resource/RedisResouce.java
@@ -1,18 +1,14 @@
 package com.example.pentaho.resource;
 
 import com.example.pentaho.component.SingleQueryDTO;
+import com.example.pentaho.service.RedisService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.ListOperations;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/redis")
@@ -20,12 +16,8 @@ public class RedisResouce {
 
     private static Logger log = LoggerFactory.getLogger(RedisResouce.class);
 
-
     @Autowired
-    private StringRedisTemplate stringRedisTemplate;
-
-    @Autowired
-    private RedisTemplate redisTemplate;
+    private RedisService redisService;
 
 
     /**
@@ -33,26 +25,25 @@ public class RedisResouce {
      */
     @PostMapping("/query-data")
     public void queryData(@RequestBody SingleQueryDTO singleQueryDTO) {
-        String redisValue = stringRedisTemplate.opsForValue().get(singleQueryDTO.getRedisKey());
-        log.info("redisValue: {}", redisValue);
+        redisService.findByKey(singleQueryDTO);
     }
+
 
     /**
      * 獲取一個key的多筆資料(RPUSH、LRANGE )
      */
     @PostMapping("/query-data-list")
-    public void queryDataList(@RequestBody SingleQueryDTO singleQueryDTO) {
-        ListOperations<String, String> listOps = stringRedisTemplate.opsForList();
-        List<String> elements = listOps.range(singleQueryDTO.getRedisKey(), 0, -1);
-        log.info("redisValue: {}", elements);
+    public void findAddr(@RequestBody SingleQueryDTO singleQueryDTO) {
+        redisService.findListByKey(singleQueryDTO);
     }
 
-    //用Jedis獲取redis，多線程中比較不安全
-//    @PostMapping("/query-data-by-jedis")
-//    public void queryDataByJedis() {
-//        Jedis jedis = new Jedis("34.211.215.66", 8005);
-//        String value = jedis.get("test");
-//        System.out.println("Redis中的值：" + value);
-//        jedis.close();
-//    }
+
+    /**
+     * 模糊比對，找出相符的 KEY (scan)
+     */
+    @PostMapping("/scan-data-list")
+    public void scanAddr(@RequestBody SingleQueryDTO singleQueryDTO) {
+        redisService.findListByScan(singleQueryDTO);
+    }
+
 }
---
+++ src/main/java/com/example/pentaho/service/RedisService.java
@@ -0,0 +1,66 @@
+package com.example.pentaho.service;
+
+import com.example.pentaho.component.SingleQueryDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.*;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+@Service
+public class RedisService {
+
+    private static Logger log = LoggerFactory.getLogger(RedisService.class);
+
+    Integer SCAN_SIZE = 1000;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+
+    /**
+     * 找單一個值
+     */
+    public void findByKey(SingleQueryDTO singleQueryDTO) {
+        String redisValue = stringRedisTemplate.opsForValue().get(singleQueryDTO.getRedisKey());
+        log.info("redisValue: {}", redisValue);
+    }
+
+
+    /**
+     * 找為LIST的值
+     */
+    public void findListByKey(SingleQueryDTO singleQueryDTO) {
+        ListOperations<String, String> listOps = stringRedisTemplate.opsForList();
+        List<String> elements = listOps.range(singleQueryDTO.getRedisKey(), 0, -1);
+        log.info("elements:{}", elements);
+    }
+
+    /**
+     * 模糊比對，找出相符的 KEY
+     */
+    public void findListByScan(SingleQueryDTO singleQueryDTO) {
+        Set<String> keySet = stringRedisTemplate.execute((RedisCallback<Set<String>>) connection -> {
+            Set<String> keySetTemp = new ConcurrentSkipListSet<>();
+            try (Cursor<byte[]> cursor = connection.scan(ScanOptions.scanOptions()
+                    .match(singleQueryDTO.getRedisKey() + "*") //模糊比對
+                    .count(SCAN_SIZE)
+                    .build())) {
+                while (cursor.hasNext() && keySetTemp.size() < SCAN_SIZE) {
+                    keySetTemp.add(new String(cursor.next(), "utf-8"));
+                }
+            } catch (Exception e) {
+                log.error("redis，模糊比對錯誤:{}", e.getMessage());
+            }
+            return keySetTemp;
+        });
+        log.info("keySet:{}", keySet);
+    }
+}
