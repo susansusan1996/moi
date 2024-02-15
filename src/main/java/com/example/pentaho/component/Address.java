fcf5615c3d52256f93bc9ea8f1b446157b0f4932
SusanChung <susan.chung@iisigroup.com>
2024-02-15 11:00:54 +0800

    單筆查詢: 修正地址的屬性名稱

--- src/main/java/com/example/pentaho/component/Address.java
+++ src/main/java/com/example/pentaho/component/Address.java
@@ -7,15 +7,15 @@ import java.util.regex.Pattern;
 public class Address {
 
     private String zipcode;
-    private String city;
-    private String region;
+    private String county;
+    private String town;
     private String village;
     private String neighbor;
     private String road;
     private String section;
     private String lane;
     private String alley;
-    private String no;
+    private String num;
     private String seq;
     private String floor;
     private String others;
@@ -31,12 +31,12 @@ public class Address {
         return zipcode;
     }
 
-    public String getCity() {
-        return city;
+    public String getCounty() {
+        return county;
     }
 
-    public String getRegion() {
-        return region;
+    public String getTown() {
+        return town;
     }
 
     public String getVillage() {
@@ -63,8 +63,8 @@ public class Address {
         return alley;
     }
 
-    public String getNo() {
-        return no;
+    public String getNum() {
+        return num;
     }
 
     public String getSeq() {
@@ -92,21 +92,21 @@ public class Address {
     }
 
     private void parseByRegex(String address) {
-        String pattern = "(?<zipcode>(^\\d{5}|^\\d{3})?)(?<city>\\D+?[縣市])(?<region>\\D+?(市區|鎮區|鎮市|[鄉鎮市區]))?(?<village>\\D+?[村里])?(?<neighbor>\\d+[鄰])?(?<road>\\D+?(村路|[路街道段]))?(?<section>\\D?段)?(?<lane>\\d+巷)?(?<alley>\\d+弄)?(?<no>\\d+號?)?(?<seq>-\\d+?(號))?(?<floor>\\d+樓)?(?<others>.+)?";
+        String pattern = "(?<zipcode>(^\\d{5}|^\\d{3})?)(?<county>\\D+?[縣市])(?<town>\\D+?(市區|鎮區|鎮市|[鄉鎮市區]))?(?<village>\\D+?[村里])?(?<neighbor>\\d+[鄰])?(?<road>\\D+?(村路|[路街道段]))?(?<section>\\D?段)?(?<lane>\\d+巷)?(?<alley>\\d+弄)?(?<num>\\d+號?)?(?<seq>-\\d+?(號))?(?<floor>\\d+樓)?(?<others>.+)?";
         Pattern regexPattern = Pattern.compile(pattern);
         Matcher matcher = regexPattern.matcher(address);
         if (matcher.matches()) {
             this.isParseSuccessed = true;
             this.zipcode = matcher.group("zipcode");
-            this.city = matcher.group("city");
-            this.region = matcher.group("region");
+            this.county = matcher.group("county");
+            this.town = matcher.group("town");
             this.village = matcher.group("village");
             this.neighbor = matcher.group("neighbor");
             this.road = matcher.group("road");
             this.section = matcher.group("section");
             this.lane = matcher.group("lane");
             this.alley = matcher.group("alley");
-            this.no = matcher.group("no");
+            this.num = matcher.group("num");
             this.seq = matcher.group("seq");
             this.floor = matcher.group("floor");
             this.others = matcher.group("others");
