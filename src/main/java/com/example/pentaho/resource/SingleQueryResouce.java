de843e7cdc1c7a0a85aa3e686978e1eb979b90d3
SusanChung <susan.chung@iisigroup.com>
2024-02-21 16:00:39 +0800

    新增單筆的swagger註解

--- src/main/java/com/example/pentaho/resource/SingleQueryResouce.java
+++ src/main/java/com/example/pentaho/resource/SingleQueryResouce.java
@@ -4,6 +4,10 @@ import com.example.pentaho.component.Address;
 import com.example.pentaho.component.SingleQueryDTO;
 import com.example.pentaho.service.SingleQueryService;
 import com.example.pentaho.utils.AddressParser;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +30,14 @@ public class SingleQueryResouce {
     /**
      * 拆分地址
      */
+  @Operation(description = "單筆查詢",
+            parameters = {
+                    @Parameter(in = ParameterIn.HEADER,
+                            name = "Authorization",
+                            description = "聖森的 JWT Token，用聖森的 RSAPublicKey 驗證 Token",
+                            required = true,
+                            schema = @Schema(type = "string"))}
+    )
     @GetMapping("/query-address")
     public ResponseEntity<Address> queryData(@RequestBody SingleQueryDTO singleQueryDTO) {
 //        return ResponseEntity.ok(new Address(singleQueryDTO.getOriginalAddress()));
