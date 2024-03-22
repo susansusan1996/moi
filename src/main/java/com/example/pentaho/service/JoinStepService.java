b554a5cac56c8462cb994fc2baf254275303fea0
SusanChung <susan.chung@iisigroup.com>
2024-03-22 15:08:40 +0800

    單筆: 整理join_step程式碼

--- src/main/java/com/example/pentaho/service/JoinStepService.java
+++ src/main/java/com/example/pentaho/service/JoinStepService.java
@@ -1,10 +1,10 @@
 package com.example.pentaho.service;
 
 import com.example.pentaho.component.Address;
+import com.example.pentaho.utils.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
@@ -17,162 +17,150 @@ public class JoinStepService {
     @Autowired
     private RedisService redisService;
 
-
     private static final int COUNTY_START_INDEX = 0;
     private static final int COUNTY_END_INDEX = 5;
     private static final int TOWN_START_INDEX = 5;
     private static final int TOWN_END_INDEX = 8;
     private static final int NEIGHBOR_START_INDEX = 10;
     private static final int NEIGHBOR_END_INDEX = 14;
-    private static final int VILLAGE_START_INDEX = 8; //里~鄰的INDEX
-    private static final int VILLAGE_END_INDEX = 14;  //里~鄰的INDEX
+    private static final int VILLAGE_START_INDEX = 8;
+    private static final int VILLAGE_END_INDEX = 14;
+    private static final int LAST_FIVE_INDEX = 14;
+
+    private static final String OLD_POSITION_1 = "32";
+    private static final String NEW_POSITION_1 = "24";
+    private static final String OLD_POSITION_2 = "24";
+    private static final String NEW_POSITION_2 = "20";
+
 
     public Set<String> findJoinStep(Address address, Set<String> newMappingIdSet, Set<String> seqSet) {
         String mappingId = address.getMappingId();
         String seq = "";
-        //最嚴謹
-        String JA112_NO_COUNTY = removeMiddleChars(mappingId, COUNTY_START_INDEX, COUNTY_END_INDEX); //未含"縣市"，先歸在"JA112"
-        String JA112 = removeMiddleChars(mappingId, TOWN_START_INDEX, TOWN_END_INDEX); //未含鄉鎮市區
-        //退鄰(鄰 挖掉，看比不比得到)
-        String JA211 = removeMiddleChars(mappingId, NEIGHBOR_START_INDEX, NEIGHBOR_END_INDEX); //含鄉鎮市區
-        String JA212 = removeMiddleChars(removeMiddleChars(mappingId, NEIGHBOR_START_INDEX, NEIGHBOR_END_INDEX), TOWN_START_INDEX, TOWN_END_INDEX); //未含鄉鎮市區
-        //退里(鄰、里挖掉，看比不比得到)
-        String JA311 = removeMiddleChars(mappingId, VILLAGE_START_INDEX, VILLAGE_END_INDEX); //含鄉鎮市區
-        String JA312 = removeMiddleChars(removeMiddleChars(mappingId, VILLAGE_START_INDEX, VILLAGE_END_INDEX), TOWN_START_INDEX, TOWN_END_INDEX); //未含鄉鎮市區
-        //退室(鄰、里、室挖掉，看比不比得到)
-        String JB111 = removeLastFiveChars(removeMiddleChars(mappingId, VILLAGE_START_INDEX, VILLAGE_END_INDEX)); //含鄉鎮市區
-        String JB112 = removeLastFiveChars(removeMiddleChars(removeMiddleChars(mappingId, VILLAGE_START_INDEX, VILLAGE_END_INDEX), TOWN_START_INDEX, TOWN_END_INDEX)); //未含鄉鎮市區
-        //樓之之樓(鄰、里、室挖掉，含有鄉鎮市區)
-        String JB211 = exchangePosition(removeLastFiveChars(removeMiddleChars(mappingId, VILLAGE_START_INDEX, VILLAGE_END_INDEX))); //含鄉鎮市區
-        log.info("JB211:{}", JB211);
-        String JB212 = exchangePosition(removeLastFiveChars(removeMiddleChars(removeMiddleChars(mappingId, VILLAGE_START_INDEX, VILLAGE_END_INDEX), TOWN_START_INDEX, TOWN_END_INDEX))); //未含鄉鎮市區
-
-        for (String id : newMappingIdSet) {
-            //最嚴謹
-            String newJA112NoCounty = removeMiddleChars(id, COUNTY_START_INDEX, COUNTY_END_INDEX); //最嚴謹比對(未含"縣市"，先歸在"JA112")
-            //最嚴謹，未含鄉鎮市區
-            String newJA112 = removeMiddleChars(id, TOWN_START_INDEX, TOWN_END_INDEX); //最嚴謹比對(未含鄉鎮市區)
-            //退鄰(鄰 挖掉，看比不比得到)
-            String newJA211 = removeMiddleChars(id, NEIGHBOR_START_INDEX, NEIGHBOR_END_INDEX); //含鄉鎮市區
-            String newJA212 = removeMiddleChars(removeMiddleChars(id, NEIGHBOR_START_INDEX, NEIGHBOR_END_INDEX), TOWN_START_INDEX, TOWN_END_INDEX);
-            //退里(鄰、里挖掉，看比不比得到)
-            String newJA311 = removeMiddleChars(id, VILLAGE_START_INDEX, VILLAGE_END_INDEX);
-            String newJA312 = removeMiddleChars(removeMiddleChars(id, VILLAGE_START_INDEX, VILLAGE_END_INDEX), TOWN_START_INDEX, TOWN_END_INDEX);
-            //退室(鄰、里、室挖掉，看比不比得到)
-            String newJB111 = removeLastFiveChars(removeMiddleChars(id, VILLAGE_START_INDEX, VILLAGE_END_INDEX));
-            String newJB112 = removeLastFiveChars(removeMiddleChars(removeMiddleChars(id, VILLAGE_START_INDEX, VILLAGE_END_INDEX), TOWN_START_INDEX, TOWN_END_INDEX));
-
-            //樓之之樓(鄰、里、室挖掉，含有鄉鎮市區)
-            String newJB211 = removeLastFiveChars(removeMiddleChars(id, VILLAGE_START_INDEX, VILLAGE_END_INDEX));
-            log.info("newJB211:{}", newJB211);
-            String newJB212 = removeLastFiveChars(removeMiddleChars(removeMiddleChars(id, VILLAGE_START_INDEX, VILLAGE_END_INDEX), TOWN_START_INDEX, TOWN_END_INDEX));
-            if (JA112_NO_COUNTY.equals(newJA112NoCounty)) {
-                address.setJoinStep("JA112");
-                seqSet.add(redisService.findByKey("最嚴謹比對(未含\"縣市\"，先歸在\"JA112\")", id, null));
+        String[] steps = {
+                "JA112_NO_COUNTY", //未含"縣市"，先歸在"JA112"
+                "JA112", //最嚴謹，未含鄉鎮市區
+                "JA211", "JA212",
+                "JA311", "JA312",
+                "JB111", "JB112",
+                "JB211", "JB212"};
+        for (String step : steps) {
+            String[][] indices = getIndices(step);
+            String id = removeChars(mappingId, indices, step);
+            String newId;
+            if (address.getJoinStep() != null) {
                 break;
-            } else if (JA112.equals(newJA112)) {
-                address.setJoinStep("JA112");
-                seq = redisService.findByKey("最嚴謹，未含鄉鎮市區", id, null);
-                seqSet.add(seq);
-                break;
-            } else if (JA211.equals(newJA211)) {
-                address.setJoinStep("JA211");
-                seq = redisService.findByKey("退鄰，含鄉鎮市區", id, null);
-                seqSet.add(seq);
-                break;
-            } else if (JA212.equals(newJA212)) {
-                address.setJoinStep("JA212");
-                seq = redisService.findByKey("退鄰，不含鄉鎮市區", id, null);
-                seqSet.add(seq);
-                break;
-            } else if (JA311.equals(newJA311)) {
-                address.setJoinStep("JA311");
-                seq = redisService.findByKey("退里，含鄉鎮市區", id, null);
-                seqSet.add(seq);
-                break;
-            } else if (JA312.equals(newJA312)) {
-                address.setJoinStep("JA312");
-                seq = redisService.findByKey("退里，不含鄉鎮市區", id, null);
-                seqSet.add(seq);
-                break;
-            } else if (JB111.equals(newJB111)) {
-                address.setJoinStep("JB111");
-                seq = redisService.findByKey("退室，含鄉鎮市區", id, null);
-                seqSet.add(seq);
-            } else if (JB112.equals(newJB112)) {
-                address.setJoinStep("JB112");
-                seq = redisService.findByKey("退室，不含鄉鎮市區", id, null);
-                seqSet.add(seq);
-            } else if (JB211.equals(newJB211)) {
-                address.setJoinStep("JB211");
-                seq = redisService.findByKey("樓之之樓，含鄉鎮市區", id, null);
-                seqSet.add(seq);
-                break;
-            } else if (JB212.equals(newJB212)) {
-                address.setJoinStep("JB212");
-                seq = redisService.findByKey("樓之之樓，不含鄉鎮市區", id, null);
-                seqSet.add(seq);
-                break;
-            } else {
-                log.info("甚麼都沒有比到!!");
-                seqSet.add(redisService.findByKey("甚麼都沒有比到，不退，找找看", id, null));
+            }
+            for (String newMappingId : newMappingIdSet) {
+                newId = removeChars(newMappingId, indices, step);
+                if (id.equals(newId)) {
+                    if ("JA112_NO_COUNTY".equals(step)) {
+                        step = "JA112";
+                    }
+                    seq = redisService.findByKey("退" + step, newMappingId, "");
+                    if (StringUtils.isNotNullOrEmpty(seq)) {
+                        seqSet.add(seq);
+                        address.setJoinStep(step);
+                        //除了退室，有可能造成多址，其他有找到seq就可以停止loop
+                        if (!"JB111".equals(step) && !"JB112".equals(step)) {
+                            break;
+                        }
+                    }
+                } else {
+                    seq = redisService.findByKey("不退，找找看", newId, "");
+                    if (StringUtils.isNotNullOrEmpty(seq)) {
+                        seqSet.add(seq);
+                    }
+                }
             }
         }
-        if (address.getJoinStep() != null) {
-            // 退室多址(退室，且不只一筆)
-            if (seqSet.size() > 1) {
-                if ("JB111".equals(address.getJoinStep()) || "JB112".equals(address.getJoinStep())) {
-                    address.setJoinStep("JD311");
-                } else {
-                    seqSet.clear();
-                    seqSet.add(seq);//留有比對到JOIN_STEP的那筆即可
-                }
+
+        if (address.getJoinStep() != null && seqSet.size() > 1) {
+            if ("JB111".equals(address.getJoinStep()) || "JB112".equals(address.getJoinStep())) {
+                address.setJoinStep("JD311");
+            } else {
+                seqSet.clear();
+                seqSet.add(seq);
             }
         }
         return seqSet;
     }
 
-
-    public static String removeMiddleChars(String str, int start, int end) {
-        if (start < 0 || end > str.length() || start >= end) {
-            // 如果起始所有小於0，結束索引大於字符串長度，或起始索引大於等於結束索引，則返回原始字串
-            return str;
-        }
-        String leftPart = str.substring(0, start);
-        String rightPart = str.substring(end);
-        return leftPart + rightPart;
+    private String[][] getIndices(String step) {
+        return switch (step) {
+            case "JA112_NO_COUNTY" ->
+                    new String[][]{{Integer.toString(COUNTY_START_INDEX), Integer.toString(COUNTY_END_INDEX)}};
+            case "JA112" -> //未含"縣市"，先歸在"JA112"
+                    new String[][]{{Integer.toString(TOWN_START_INDEX), Integer.toString(TOWN_END_INDEX)}};
+            case "JA211" ->  //退鄰(鄰挖掉)，含鄉鎮市區
+                    new String[][]{{Integer.toString(NEIGHBOR_START_INDEX), Integer.toString(NEIGHBOR_END_INDEX)}};
+            case "JA212" ->  //退鄰(鄰挖掉)，不含鄉鎮市區
+                    new String[][]{
+                            {Integer.toString(NEIGHBOR_START_INDEX), Integer.toString(NEIGHBOR_END_INDEX)},
+                            {Integer.toString(TOWN_START_INDEX), Integer.toString(TOWN_END_INDEX)}};
+            case "JA311" ->  //退里(鄰、里挖掉)，含鄉鎮市區
+                    new String[][]{{Integer.toString(VILLAGE_START_INDEX), Integer.toString(VILLAGE_END_INDEX)}};
+            case "JA312" ->  //退里(鄰、里挖掉)，不含鄉鎮市區
+                    new String[][]{
+                            {Integer.toString(VILLAGE_START_INDEX), Integer.toString(VILLAGE_END_INDEX)},
+                            {Integer.toString(TOWN_START_INDEX), Integer.toString(TOWN_END_INDEX)}
+                    };
+            case "JB111", "JB211" ->  //JB111: 退室(鄰、里、室挖掉)，含鄉鎮市區 //JB211: 樓之之樓，含鄉鎮市區
+                    new String[][]{
+                            {Integer.toString(VILLAGE_START_INDEX), Integer.toString(VILLAGE_END_INDEX)},
+                            {Integer.toString(LAST_FIVE_INDEX)}};
+            case "JB112", "JB212" ->  //JB112: 退室(鄰、里、室挖掉)，不含鄉鎮市區 //JB212: 樓之之樓，不含鄉鎮市區
+                    new String[][]{
+                            {Integer.toString(VILLAGE_START_INDEX), Integer.toString(VILLAGE_END_INDEX)},
+                            {Integer.toString(TOWN_START_INDEX), Integer.toString(TOWN_END_INDEX)},
+                            {Integer.toString(LAST_FIVE_INDEX)}};
+            default -> new String[][]{};
+        };
     }
 
-    public static String removeLastFiveChars(String str) {
-        if (str.length() <= 5) {
-            return "";
+
+    private String removeChars(String str, String[][] indices, String type) {
+        StringBuilder builder = new StringBuilder(str);
+        for (String[] index : indices) {
+            if (index.length == 2) {
+                builder.delete(Integer.parseInt(index[0]), Integer.parseInt(index[1]));
+            } else if (index.length == 1) {
+                builder = new StringBuilder(builder.substring(0, builder.length() - 5));
+            }
         }
-        return str.substring(0, str.length() - 5);
+        //要交換POSITION
+        if (type.startsWith("JB")) {
+            builder = new StringBuilder(exchangePosition(builder.toString(), type));
+        }
+        return builder.toString();
     }
+
 
     //如果position欄位有 32xxx、x32xx、xx32x、xxx32 (32連在一起的)，就把position改成23xxx、x23xx、xx23x、xxx23
-    public static String exchangePosition(String str) {
-        log.info("原始 position mapping:{}", str);
-        StringBuilder result = new StringBuilder();
-        if (str.length() < 5) {
-            return str;
+    private static String exchangePosition(String positionMapping, String type) {
+        if (positionMapping == null || positionMapping.length() < 5) {
+            return positionMapping;
         }
-        // 將前面的字符直接添加進結果中
-        result.append(str, 0, str.length() - 5);
-        // 檢查最後五個字是否有32
-        String lastFive = str.substring(str.length() - 5);
-        if (lastFive.contains("32")) {
-            // 如果存在，則進行交換
-            int index = lastFive.indexOf("32");
-            result.append(lastFive.substring(0, index)); // 將 32 前面的字符添加到结果中
-            result.append("24"); // replace成" 七樓 之１"
-            result.append(lastFive.substring(index + 2)); // 將 32 后面的字符添加到结果中
-        } else {
-            // 如果不存在，直接把最後五個字元添加回字串
-            result.append(lastFive);
+        String oldPosition;
+        String newPosition;
+        switch (type) {
+            //樓之之樓
+            case "JB211":
+            case "JB212":
+                oldPosition = OLD_POSITION_1;
+                newPosition = NEW_POSITION_1;
+                break;
+            //退樓後之
+            case "JB311":
+            case "JB312":
+                oldPosition = OLD_POSITION_2;
+                newPosition = NEW_POSITION_2;
+                break;
+            default:
+                return positionMapping;
         }
-        log.info("交換後的 position mapping:{}", result.toString());
-        return result.toString();
+        String lastFive = positionMapping.substring(positionMapping.length() - 5);
+        String updatedLastFive = lastFive.replace(oldPosition, newPosition);
+        return positionMapping.replaceAll("\\d{5}$", updatedLastFive);
     }
 
 }
