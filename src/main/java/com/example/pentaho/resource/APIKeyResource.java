5b021ebac4caefa5e662ea5c29c49d0ca192bd0f
YuNung <860102yeah@gmail.com>
2024-03-29 12:32:38 +0800

    新增iisi(AWS),uat(內政測試),yaml檔 新增單筆軌跡對外程式

--- src/main/java/com/example/pentaho/resource/APIKeyResource.java
+++ src/main/java/com/example/pentaho/resource/APIKeyResource.java
@@ -1,9 +1,11 @@
 package com.example.pentaho.resource;
 
+import com.example.pentaho.component.IbdTbIhChangeDoorplateHis;
 import com.example.pentaho.component.KeyComponent;
 import com.example.pentaho.component.Token;
 import com.example.pentaho.component.User;
 import com.example.pentaho.exception.MoiException;
+import com.example.pentaho.service.SingleTrackQueryService;
 import com.example.pentaho.utils.RSAJWTUtils;
 import com.example.pentaho.utils.RsaUtils;
 import com.example.pentaho.utils.UserContextUtils;
@@ -20,10 +22,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.PrivateKey;
+import java.util.List;
 
 /***
  * 產生APIKey & 使用APIKey驗證的API
@@ -36,7 +40,11 @@ public class APIKeyResource {
     /**API效期先設定1天**/
     private static final int VALID_TIME = 1440;
 
+    @Autowired
+    private SingleTrackQueryService singleQueryTrackService;
+
     private KeyComponent keyComponent;
+
 
     public APIKeyResource(KeyComponent keyComponent) {
         this.keyComponent = keyComponent;
@@ -93,11 +101,32 @@ public class APIKeyResource {
 
 
     /**
-     * 單筆未登入測試
+     * 單筆軌跡未登入測試
      */
-    @Operation(description = "單筆未登入測試")
-    @PostMapping("/forguest")
-    public ResponseEntity<String> forGuestUser(){
-        return new ResponseEntity<>("用戶未登入", HttpStatus.OK);
+    @Operation(description = "單筆軌跡未登入",
+            parameters = {@Parameter(in = ParameterIn.HEADER,
+                    name = "Authorization",
+                    description = "jwt token,body附帶 userInfo={\"Id\":1,\"orgId\":\"Admin\"}",
+                    required = true,
+                    schema = @Schema(type = "string"))
+            },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "",
+                            content = @Content(schema = @Schema(implementation = IbdTbIhChangeDoorplateHis.class))),
+                    @ApiResponse(responseCode = "500", description = "",
+                            content = @Content(schema = @Schema(implementation = String.class), examples = @ExampleObject(value = ""))
+                    )})
+    @PostMapping("/query-track-for-guest")
+    public ResponseEntity<List<IbdTbIhChangeDoorplateHis>> queryTrack(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "編碼",
+                    required = true,
+                    content = @Content(
+                            schema = @Schema(implementation = String.class),
+                            examples = @ExampleObject(value = "BSZ7538-0")
+                    )
+            )
+            @RequestBody String addressId) {
+        return new ResponseEntity<>(singleQueryTrackService.querySingleTrack(addressId), HttpStatus.OK);
     }
 }
