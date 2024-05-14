83d5fa17445866edaafb5feffe84ee700a4e0eb0
SusanChung <susan.chung@iisigroup.com>
2024-05-14 10:01:39 +0800

    單筆: 修正切地址的正則

--- src/main/java/com/example/pentaho/utils/AddressParser.java
+++ src/main/java/com/example/pentaho/utils/AddressParser.java
@@ -38,10 +38,11 @@ public class AddressParser {
     private final String DYNAMIC_ALLEY_PART = "|卓厝|安農新邨|吉祥園|蕭厝|泰安新村|美喬|１弄圳東|堤外|中興二村|溝邊|長埤|清水|南苑|二橫路|朝安|黃泥塘|建行新村|牛頭|永和山莊";
     private final String COUNTY = "(?<zipcode>(^\\d{5}|^\\d{3})?)(?<county>.*縣|.*市|%s)?";
     private final String TOWN = "(?<town>\\D+?(市區|鎮區|鎮市|[鄉鎮市區])|%s)?";
-    private final String VILLAGE = "(?<village>\\D+?(?:[村里]+|村|里)|%s)?";
+    private final String VILLAGE = "(?<village>.*村里|.*?村|.*?里|%s)?";
     private final String NEIGHBOR = "(?<neighbor>" + ALL_CHAR + "+鄰)?";
-    private final String ROAD = "(?<road>.+段(?!.*巷)|.+街(?!.*巷)|.+大道(?!.*巷)|.+路(?!.*巷)|%s(?!.*巷))?";
-    private final String LANE = "(?<lane>.+巷)?";
+    private final String SPECIALLANE = "(?<speciallane>鐵路.*巷|丹路.*巷)?"; //避免被切到路，直接先寫死在這裡
+    private final String ROAD = "(?<road>(.*段|.*街|.*大道|.*?路(?!巷)|%s)?)";
+    private final String LANE = "(?<lane>.*巷)?";
     private final String ALLEY = "(?<alley>" + ALL_CHAR + "+弄" + DYNAMIC_ALLEY_PART + ")?";
     private final String SUBALLEY = "(?<subAlley>" + ALL_CHAR + "+[衖衕橫])?";
     private final String NUMFLR1 = "(?<numFlr1>" + ALL_CHAR + "+[號Ff樓之-區棟]|" + BASEMENT_PATTERN + ")?";
@@ -50,7 +51,7 @@ public class AddressParser {
     private final String NUMFLR4 = "(?<numFlr4>[之-]+" + ALL_CHAR + "+(?!.*[樓FｆＦf])|" + ALL_CHAR+"+[FｆＦf]|"+ ALL_CHAR + "+[號樓FｆＦf之-區棟]|" + BASEMENT_PATTERN + "|" + ALL_CHAR + "+(?!室))?";
     private final String NUMFLR5 = "(?<numFlr5>[之-]+" + ALL_CHAR + "+(?!.*[樓FｆＦf])|" + BASEMENT_PATTERN + ")?";
     private final String CONTINUOUS_NUM = "(?<continuousNum>[之-]+.*[樓FｆＦf])?"; //之45一樓
-    private final String ROOM = "(?<room>" + ALL_CHAR + "+室)?";
+    private final String ROOM = "(?<room>.*室)?";
     private final String BASEMENTSTR = "(?<basementStr>屋頂突出.*層|地下.*層|地下.*樓|地下|地下室|底層|屋頂|頂樓|屋頂突出物|屋頂樓|頂層)?";
     private final String ADDRREMAINS = "(?<addrRemains>.+)?";
 
@@ -161,7 +162,7 @@ public class AddressParser {
         String newTown = String.format(TOWN , String.join("|",townList));
         String newVillage = String.format(VILLAGE , String.join("|",villageList));
         String newRoad = String.format(ROAD , String.join("|",roadList));
-        String finalPattern = newCounty + newTown + newVillage + NEIGHBOR + newRoad + LANE + ALLEY + SUBALLEY + NUMFLR1 + NUMFLR2 + NUMFLR3 + NUMFLR4 + NUMFLR5 + CONTINUOUS_NUM + ROOM + BASEMENTSTR + ADDRREMAINS;
+        String finalPattern = newCounty + newTown + newVillage + NEIGHBOR + SPECIALLANE + newRoad + LANE + ALLEY + SUBALLEY + NUMFLR1 + NUMFLR2 + NUMFLR3 + NUMFLR4 + NUMFLR5 + CONTINUOUS_NUM + ROOM + BASEMENTSTR + ADDRREMAINS;
 //        log.info("finalPattern==>{}",finalPattern);
         return finalPattern;
     }
@@ -187,7 +188,7 @@ public class AddressParser {
         address.setVillage(matcher.group("village"));
         address.setNeighbor(matcher.group("neighbor"));
         address.setRoad(matcher.group("road"));
-        address.setLane(matcher.group("lane"));
+        address.setLane(matcher.group("speciallane") != null ? matcher.group("speciallane") : matcher.group("lane"));
         address.setAlley(matcher.group("alley"));
         address.setSubAlley(matcher.group("subAlley"));
         address.setNumFlr1(matcher.group("numFlr1"));
